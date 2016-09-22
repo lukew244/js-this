7a0f00140568649277809c78120e41b0ff9f6324
lukew244 <luke.webster86@gmail.com>
2016-09-22 16:52:33 +0100

    exercise

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello!");
+function exclaimName() {
+  // What is the value of `this` the first time `exclaimName` is run?
+  // What about the second time?
+  // What about the third time?
+  // Research project: what's the difference between call and apply?
+
+  return this.name + "!";
+};
+
+console.log(exclaimName());
+
+var exclaimMary = exclaimName.call({ name: "Mary" });
+console.log(exclaimMary);
+
+var exclaimIsla = exclaimName.apply({ name: "Isla" });
+console.log(exclaimIsla);
